c5f94baea36ce87983f0e8be696fa3b8de7fb96c
Nick McCally <nick@amida.com>
2020-02-06 16:49:45 -0500

    Update class-wp-bootstrap-navwalker.php

--- inc/class-wp-bootstrap-navwalker.php
+++ inc/class-wp-bootstrap-navwalker.php
@@ -145,7 +145,7 @@ if ( ! class_exists( 'Understrap_WP_Bootstrap_Navwalker' ) ) {
 
 			// Add some additional default classes to the item.
 			$classes[] = 'menu-item-' . $item->ID;
-			$classes[] = 'nav-item text-md-right';
+			$classes[] = 'nav-item text-md-right text-xs-left';
 
 			// Allow filtering the classes.
 			$classes = apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args, $depth );
