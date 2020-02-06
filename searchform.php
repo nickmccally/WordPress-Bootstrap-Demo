3ea5c6f672a9440c6d6347ef043d21cddb52bca3
Nick McCally <nick@amida.com>
2020-02-06 12:16:40 -0500

    Theme background color updates

--- searchform.php
+++ searchform.php
@@ -15,7 +15,7 @@ defined( 'ABSPATH' ) || exit;
 		<input class="field form-control" id="s" name="s" type="text"
 			placeholder="<?php esc_attr_e( 'Search &hellip;', 'understrap' ); ?>" value="<?php the_search_query(); ?>">
 		<span class="input-group-append">
-			<input class="submit btn btn-primary" id="searchsubmit" name="submit" type="submit"
+			<input class="submit btn btn-secondary" id="searchsubmit" name="submit" type="submit"
 			value="<?php esc_attr_e( 'Search', 'understrap' ); ?>">
 		</span>
 	</div>
