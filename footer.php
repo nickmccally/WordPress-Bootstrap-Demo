1b020390624652f9d5b19083fe03143cc8266e6d
Nick McCally <nick@amida.com>
2020-02-05 22:40:55 -0500

    update footer

--- footer.php
+++ footer.php
@@ -27,7 +27,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 					<div class="site-info">
 
-						<?php understrap_site_info(); ?>
+						
 
 					</div><!-- .site-info -->
 
@@ -48,4 +48,3 @@ $container = get_theme_mod( 'understrap_container_type' );
 </body>
 
 </html>
-
